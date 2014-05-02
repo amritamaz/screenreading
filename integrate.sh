34f5fc009db8fe9a4acb7809186acfdeca5b8ce3
Amrita Mazumdar <amritamaz@gmail.com>
2014-05-02 13:24:19 -0400

    trying to convert out unicode rn

--- integrate.sh
+++ integrate.sh
@@ -3,3 +3,6 @@
 echo "Integrating all TXTs to all.txt"
 
 cat corpus/*.txt >> corpus/all.txt
+#convert encoding
+echo "Converting to Ascii"
+iconv -f utf8 -t US-ASCII corpus/all.txt corpus/all_ascii.txt
