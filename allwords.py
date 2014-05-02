12cf211b41374ddc419ba278bfdc29b9f54b5758
Amrita Mazumdar <amritamaz@gmail.com>
2014-05-02 13:23:48 -0400

    comments and organization

--- allwords.py
+++ allwords.py
@@ -9,11 +9,19 @@ all = open('corpus/all.txt').read()
 
 # normalize our corpus
 tokens = nltk.word_tokenize(all)
+
+# lowercase
 words = [w.lower() for w in tokens]
+
+# remove stopwords
 words = [w for w in words if not w in stopwords.words('english')]
+
+# remove 1 and 2 letter words
 words = [w for w in words if len(w)!=1]
 words = [w for w in words if len(w)!=2]
 
+# remove unicode characters
+words = [w.decode('unicode_escape').encode('ascii','ignore') for w in words]
 vocab = sorted(set(words))
 
 # let's find the most frequent words
