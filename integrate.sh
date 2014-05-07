fbd29ca876b6305ed5c895dccf7c9c5c19de30f9
Amrita Mazumdar <amritamaz@gmail.com>
2014-05-07 16:13:32 -0400

    added ascii conversion to integrate file

--- integrate.sh
+++ integrate.sh
@@ -2,7 +2,8 @@
 
 echo "Integrating all TXTs to all.txt"
 
-cat corpus/*.txt >> corpus/all.txt
+rm corpus/all.txt > /dev/null
+cat corpus/*.txt > corpus/all.txt
 #convert encoding
 echo "Converting to Ascii"
-iconv -f utf8 -t US-ASCII corpus/all.txt corpus/all_ascii.txt
+iconv -cs -f utf8 -t US-ASCII corpus/all.txt > corpus/all_ascii.txt
